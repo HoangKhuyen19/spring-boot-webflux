9d41225f358ebc4042ba283aa536862e4a3c158b
HoangKhuyen19 <104675608+HoangKhuyen19@users.noreply.github.com>
2024-03-28 20:09:14 +0700

    Modify response return in the repository.

--- src/main/java/com/khuyen/springbootwebflux/model/EmployeeRepository.java
+++ src/main/java/com/khuyen/springbootwebflux/model/EmployeeRepository.java
@@ -3,97 +3,235 @@ package com.khuyen.springbootwebflux.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+
+import com.khuyen.springbootwebflux.response.Response;
+
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 public class EmployeeRepository {
-    //Static fields:
+    // Static fields:
     private static final int LIMIT = 10000;
     public static EmployeeRepository instance;
 
-    //Static method:
+    // Static method:
     public static EmployeeRepository getInstance() {
         if (instance == null) {
-            instance =  new EmployeeRepository();
+            instance = new EmployeeRepository();
         }
 
         return instance;
     }
 
-    //Fields:
+    // Fields:
     private List<Employee> employees;
     private Disposable generateEmployeesDisposable;
 
-    //Constructor:
+    // Constructor:
     public EmployeeRepository() {
         employees = new ArrayList<>();
 
         generateEmployeesDisposable = Flux.range(0, LIMIT)
-        .publishOn(Schedulers.parallel())
-        .subscribe(
-            id -> {
-                employees.add(
-                    new Employee(id, "Employee " + id)
-                );
-                System.out.println("Added employee with ID: " + id);
-                
-                try {
-                    Thread.sleep(100);
-                }
-                catch (Exception e) {
+                .publishOn(Schedulers.parallel())
+                .subscribe(
+                        id -> {
+                            employees.add(
+                                    new Employee(id, "Employee " + id));
+                            System.out.println("Added employee with ID: " + id);
+
+                            try {
+                                Thread.sleep(100);
+                            } catch (Exception e) {
+                            }
+                        });
+    }
+
+    // Methods:
+    public Mono<Response<List<Employee>>> getAll() {
+        return Mono.fromSupplier(() -> {
+            Response<List<Employee>> response = new Response<>();
+
+            response.setSuccess(true);
+            response.setResult(employees);
+            return response;
+        }).publishOn(Schedulers.parallel());
+    }
+
+    public Mono<Response<Employee>> get(String idStr) {
+        return Mono.fromSupplier(() -> {
+            Response<Employee> response = new Response<>();
+
+            int id;
+            try {
+                id = Integer.parseInt(idStr);
+            } catch (Exception e) {
+                response.setSuccess(false);
+                response.setMessage(
+                        "ID must be an integer!");
+                response.setCode(HttpStatus.BAD_REQUEST.toString());
+                return response;
+            }
+
+            Employee employee = this.getEmployee(id);
+
+            if (employee == null) {
+                response.setSuccess(true);
+                response.setMessage("Employee not found!");
+                response.setCode("EMPLOYEE_NOTFOUND");
+                return response;
+            }
+
+            response.setSuccess(true);
+            response.setResult(employee);
+            return response;
+        }).publishOn(Schedulers.parallel());
+    }
+
+    public Mono<Response<List<Employee>>> getByKeyword(String keyword) {
+        return Mono.fromSupplier(() -> {
+            Response<List<Employee>> response = new Response<>();
+
+            if (keyword == null) {
+                response.setSuccess(false);
+                response.setMessage("Key word cannot be null!");
+                return response;
+            }
+
+            if (keyword.equals("")) {
+                response.setSuccess(false);
+                response.setMessage("Keyword cannot be empty!");
+                return response;
+            }
+
+            List<Employee> result = new ArrayList<>();
+
+            for (Employee employee : employees) {
+                if (employee.toString().toLowerCase().contains(keyword.toLowerCase())) {
+                    result.add(employee);
                 }
             }
-        );
+
+            response.setSuccess(true);
+            response.setResult(result);
+            return response;
+        }).publishOn(Schedulers.parallel());
     }
 
-    //Methods:
-    public List<Employee> getAll() {
-        return employees;
+    public Mono<Response<Void>> insert(Employee employee) {
+
+        return Mono.fromSupplier(() -> {
+            Response<Void> response = new Response<>();
+
+            if (this.getEmployee(employee.getId()) != null) {
+                response.setSuccess(false);
+                response.setMessage("Employee with given id already exist!");
+                response.setCode("EMPLOYEE_ALREADY_EXIST");
+                return response;
+            }
+            employees.add(employee);
+            response.setSuccess(true);
+
+            return response;
+        });
     }
 
-    public Employee get(int id) {
-        for(Employee employee : this.employees) {
-            if(employee.getId() == id) {
+    public Mono<Response<Void>> update(String idStr, Employee employee) {
+
+        return Mono.fromSupplier(() -> {
+            Response<Void> response = new Response<>();
+
+            int id;
+            try {
+                id = Integer.parseInt(idStr);
+            } catch (Exception e) {
+                response.setSuccess(false);
+                response.setMessage("ID must be an integer!");
+                response.setCode("ID_INVALID");
+                return response;
+            }
+
+            Employee target = this.getEmployee(id);
+
+            if (target == null) {
+                response.setSuccess(false);
+                response.setMessage("Employee with given id doesn't exist!");
+                response.setCode("EMPLOYEE_NOT_EXIST");
+                return response;
+            }
+
+            target.setName(employee.getName());
+            response.setSuccess(true);
+            return response;
+        }).publishOn(Schedulers.parallel());
+    }
+
+    public Mono<Response<Void>> delete(String idStr) {
+        return Mono.fromSupplier(() -> {
+            Response<Void> response = new Response<>();
+
+            int id;
+            try {
+                id = Integer.parseInt(idStr);
+            } catch (Exception e) {
+                response.setSuccess(false);
+                response.setMessage("ID must be an integer!");
+                response.setCode("ID_INVALID");
+                return response;
+            }
+
+            if (this.getEmployee(id) == null) {
+                response.setSuccess(false);
+                response.setMessage("Employee with given id doesn't exist!");
+                response.setCode("EMPLOYEE_NOT_EXIST");
+                return response;
+            }
+
+            for (Employee employee : this.employees) {
+                if (employee.getId() == id) {
+                    employees.remove(employee);
+                    break;
+                }
+            }
+
+            response.setSuccess(true);
+            return response;
+        });
+    }
+
+    public Mono<Response<Void>> cancelGenerateEmployees() {
+        return Mono.fromSupplier(() -> {
+            Response<Void> response = new Response<Void>();
+
+            try {
+                if (!generateEmployeesDisposable.isDisposed()) {
+                    generateEmployeesDisposable.dispose();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                response.setSuccess(false);
+                response.setMessage(
+                        "Failed while canceling generate employees!");
+                response.setCode("FAILED_CANCELING_GENERATE_EMPLOYEES");
+
+                return response;
+            }
+
+            response.setSuccess(true);
+
+            return response;
+        });
+    }
+
+    private Employee getEmployee(int id) {
+        for (Employee employee : employees) {
+            if (employee.getId() == id) {
                 return employee;
             }
         }
-
         return null;
-    }
-
-    public List<Employee> getByKeyword(String keyword) {
-        List<Employee> result = new ArrayList<>();
-        for(Employee employee : employees) {
-            if(
-                employee.toString()
-                .toLowerCase()
-                .contains(
-                    keyword.toLowerCase()
-                ) 
-            ) {
-                result.add(employee);
-            }
-        }
-
-        return result;
-    }
-    public void insert(Employee employee) {
-        employees.add(employee);
-    }
-
-    public void delete(int id) {
-        for(Employee employee : this.employees) {
-            if(employee.getId() == id) {
-                employees.remove(id);
-                return;
-            }
-        }
-    }
-
-    public void cancelGenerateEmployees() {
-        if(!generateEmployeesDisposable.isDisposed()){
-            generateEmployeesDisposable.dispose();
-        }
     }
 }
--- src/main/java/com/khuyen/springbootwebflux/rest/EmployeeController.java
+++ src/main/java/com/khuyen/springbootwebflux/rest/EmployeeController.java
@@ -33,149 +33,53 @@ public class EmployeeController {
 
     @GetMapping("/{idStr}")
     public Mono<Response<Employee>> getEmployeeById(@PathVariable String idStr) {
-        Response<Employee> response = new Response<>();
-
-        int id;
-        try {
-            id = Integer.parseInt(idStr);
-        } catch (Exception e) {
-            response.setSuccess(false);
-            response.setMessage(
-                    "ID must be an integer!");
-            response.setCode(HttpStatus.BAD_REQUEST.toString());
-            return Mono.just(response);
-        }
-
-        Employee employee = employeeRepository.get(id);
-
-        if (employee == null) {
-            response.setSuccess(false);
-            response.setCode(HttpStatus.NOT_FOUND.toString());
-            response.setMessage(
-                    "Employee not found!");
-            response.setCode(HttpStatus.NOT_FOUND.toString());
-            return Mono.just(response);
-        }
-
-        response.setSuccess(true);
-        response.setResult(employee);
-        return Mono.just(response);
+       return employeeRepository.get(idStr);
     }
 
     @GetMapping
     public Mono<Response<List<Employee>>> getByKeyword(@RequestParam(name="keyword", required = false) String keyword) {
-        Response<List<Employee>> response = new Response<>();
-        response.setSuccess(true);
-        
-        if (keyword == null || keyword.isEmpty()) {
-            response.setResult(employeeRepository.getAll());
-        } else {
-           
-            response.setResult(employeeRepository.getByKeyword(keyword));
-        }
-
-        return Mono.just(response);
+        return (
+            keyword == null 
+            ? employeeRepository.getAll()
+            : employeeRepository.getByKeyword(keyword)
+        );
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Response<Void>> insert(@RequestBody Employee employee) {
-        Response<Void> response = new Response<>();
-
-        if(employeeRepository.get(employee.getId()) != null) {
-            response.setSuccess(false);
-            response.setMessage("Employee with given id already exist!");
-            response.setCode("EMPLOYEE_ALREADY_EXIST");
-            return Mono.just(response);
-        }
-
-        employeeRepository.insert(employee);
-        response.setSuccess(true);
-        return Mono.just(response);
+        return employeeRepository.insert(employee);
     }
 
     @PutMapping("/{idStr}")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Response<Void>> update(@PathVariable String idStr, @RequestBody Employee employee) {
-        Response<Void> response = new Response<>();
-
-        int id;
-        try {
-            id = Integer.parseInt(idStr);
-        }
-        catch (Exception e) {
-            response.setSuccess(false);
-            response.setMessage("ID must be an integer!");
-            response.setCode("ID_INVALID");
-            return Mono.just(response);
-        }
-
-        Employee target = employeeRepository.get(id);
-
-        if (target == null) {
-            response.setSuccess(false);
-            response.setMessage("Employee with with given id doesn't exist!");
-            response.setCode("EMPLOYEE_NOT_EXIST");
-            return Mono.just(response);
-        }
-
-        target.setName(employee.getName());
-
-        response.setSuccess(true);
-        return Mono.just(response);
+        return employeeRepository.update(idStr, employee);
     }
 
     @DeleteMapping("/generateEmployees")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Response<Void>> cancelGenerateEmployees() {
 
-        Response<Void> response = new Response<Void>();
-        
-        try {
-            employeeRepository.cancelGenerateEmployees();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-
-            response.setSuccess(false);
-            response.setMessage(
-                "Failed while canceling generate employees!"
-            );
-            response.setCode("FAILED_CANCELING_GENERATE_EMPLOYEES");
-
-            return Mono.just(response);
-        }
-        
-        response.setSuccess(true);
-
-        return Mono.just(response);
+        return employeeRepository.cancelGenerateEmployees();
     }
 
     @DeleteMapping("/{idStr}")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Response<Void>> delete(@PathVariable String idStr){
-        Response<Void> response = new Response<>();
+        return employeeRepository.delete(idStr);
+    }
 
-        int id;
-        try {
-            id = Integer.parseInt(idStr);
-        } catch (Exception e) {
-            response.setSuccess(false);
-            response.setMessage("Id must be an interger");
-            response.setCode("ID_INVALID");
-            return Mono.just(response);
-        }
-
-        if(employeeRepository.get(id) == null) {
-            response.setSuccess(false);
-            response.setMessage("Employee with given id doesn't exist!");
-            response.setCode("EMPLOYEE_NOT_EXIST");
-            return Mono.just(response);
-        }
-
-        employeeRepository.delete(id);
-
-        response.setSuccess(true);
-        return Mono.just(response);
+    @GetMapping("/test")
+    public Mono test() {
+        
+        return Mono.fromSupplier(()->{
+            throw new RuntimeException();
+        })
+        .onErrorResume(
+            e -> {
+                return Mono.just("Error");
+            }
+        );
     }
 }
